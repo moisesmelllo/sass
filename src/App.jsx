b4bf622e56c76f2c79de048aea64b87ad3a4d837
Moises Melo Viana Ferreira <moisesmvferreira@gmail.com>
2023-11-04 14:56:20 -0300

    corrigindo camilcase no import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import MainContent from './components/MainContent';
 import Sidebar from './components/Sidebar';
 
-import './styles/components/app.Sass';
+import './styles/components/app.sass';
 
 function App() {
 
